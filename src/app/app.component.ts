9f4fb9fe9d50c77386465beb29b4299487cf504e
avasquezu <avasquezu@ingenieria.edu.pe>
2026-08-11 10:21:47 -0500

    Se agrego iconos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { FontIconService } from './core/services/icon.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'front-scap';
+
+  public iconService = inject(FontIconService)
+
+  constructor() {}
 }
---
+++ src/app/core/services/icon.service.ts
@@ -0,0 +1,188 @@
+import { Injectable } from '@angular/core';
+import { faWarning, faCirclePlus } from '@fortawesome/free-solid-svg-icons';
+import {
+  faArrowLeft,
+  faArrowUp,
+  faCalendar,
+  faCalendarCheck,
+  faCalendarDays,
+  faCheckCircle,
+  faCamera,
+  faCheck,
+  faCircle,
+  faClock,
+  faClockRotateLeft,
+  faClose,
+  faCoffee,
+  faCopy,
+  faDesktop,
+  faDotCircle,
+  faEdit,
+  faEnvelope,
+  faExternalLink,
+  faEye,
+  faFile,
+  faFileDownload,
+  faFileInvoiceDollar,
+  faFilter,
+  faHandHoldingDollar,
+  faInfo,
+  faInfoCircle,
+  faInbox,
+  faList,
+  faLocationPin,
+  faPencil,
+  faPenFancy,
+  faPhone,
+  faPlus,
+  faPowerOff,
+  faRegistered,
+  faSave,
+  faSearch,
+  faSnowflake,
+  faSpinner,
+  faStore,
+  faTag,
+  faTasks,
+  faTicket,
+  faTimes,
+  faTrash,
+  faUndo,
+  faUserGroup,
+  faUsers,
+  faWarehouse,
+  faRedo,
+  faWrench,
+  faCircleCheck,
+  faCircleXmark,
+  faMessage,
+  faSpellCheck,
+  faCircleLeft,
+  faCircleRight,
+  faUserCheck,
+  faCircleExclamation,
+  faGear,
+  faTableColumns,
+  faCaretUp,
+  faCaretDown,
+  faListCheck,
+  faFlag,
+  faChevronDown,
+  faCircleInfo,
+  faX,
+  faComments,
+  faAngleDown,
+  faRandom,
+  faEraser,
+  faBookOpen,
+  faAngleLeft,
+  faShare,
+  faFilePdf,
+  faStop,
+  faUpload,
+  faUser,
+  faShield,
+  faIdCard,
+  faEyeSlash,
+  faKey,
+  faPlayCircle,
+  faUserPlus,
+  faBan,
+  faArrowRight,
+} from '@fortawesome/free-solid-svg-icons';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FontIconService {
+  public faBan = faBan;
+  public faArrowRight = faArrowRight;
+  public faCalendarDays = faCalendarDays;
+  public faTimes = faTimes;
+  public faInbox = faInbox;
+  public faCheckCircle = faCheckCircle;
+  public faAngleDown = faAngleDown;
+  public faEyeSlash = faEyeSlash;
+  public faKey = faKey;
+  public faUser = faUser;
+  public faUserPlus = faUserPlus;
+  public faIdCard = faIdCard;
+  public faShield = faShield;
+  public faUpload = faUpload;
+  public faStop = faStop;
+  public faFilePdf = faFilePdf;
+  public faShare = faShare;
+  public faAngleLeft = faAngleLeft;
+  public faBookOpen = faBookOpen;
+  public faRandom = faRandom;
+  public faEraser = faEraser;
+  public faEnvelope = faEnvelope;
+  public faEdit = faEdit;
+  public faTrash = faTrash;
+  public faCheck = faCheck;
+  public faCircle = faCircle;
+  public faPhone = faPhone;
+  public faPlus = faPlus;
+  public faFile = faFile;
+  public faPencil = faPencil;
+  public faClose = faClose;
+  public faSave = faSave;
+  public faCamera = faCamera;
+  public faEye = faEye;
+  public faDesktop = faDesktop;
+  public faInfoCircle = faInfoCircle;
+  public faUndo = faUndo;
+  public faStore = faStore;
+  public faUsers = faUsers;
+  public faWarehouse = faWarehouse;
+  public faLocationPin = faLocationPin;
+  public faSearch = faSearch;
+  public faPenFancy = faPenFancy;
+  public faList = faList;
+  public faFileInvoiceDollar = faFileInvoiceDollar;
+  public faCalendarCheck = faCalendarCheck;
+  public faSpinner = faSpinner;
+  public faRegistered = faRegistered;
+  public faHandHoldingDollar = faHandHoldingDollar;
+  public faClock = faClock;
+  public faPowerOff = faPowerOff;
+  public faFileDownload = faFileDownload;
+  public faClockRotateLeft = faClockRotateLeft;
+  public faFilter = faFilter;
+  public faTicket = faTicket;
+  public faUserGroup = faUserGroup;
+  public faExternalLink = faExternalLink;
+  public faCopy = faCopy;
+  public faTasks = faTasks;
+  public faDotCircle = faDotCircle;
+  public faCalendar = faCalendar;
+  public faWrench = faWrench;
+  public faArrowLeft = faArrowLeft;
+  public faTag = faTag;
+  public faArrowUp = faArrowUp;
+  public faSnowflake = faSnowflake;
+  public faInfo = faInfo;
+  public faWarning = faWarning;
+  public faCoffee = faCoffee;
+  public faCirclePlus = faCirclePlus;
+  public faCircleCheck = faCircleCheck;
+  public faCircleXmark = faCircleXmark;
+  public faMessage = faMessage;
+  public faSpellCheck = faSpellCheck;
+  public faCircleLeft = faCircleLeft;
+  public faCircleRight = faCircleRight;
+  public faUserCheck = faUserCheck;
+  public faCircleExclamacion = faCircleExclamation;
+  public faGear = faGear;
+  public faTableColumns = faTableColumns;
+  public faCaretUp = faCaretUp;
+  public faCaretDown = faCaretDown;
+  public faListCheck = faListCheck;
+  public faFlag = faFlag;
+  public faChevronDown = faChevronDown;
+  public faCircleInfo = faCircleInfo;
+  public faX = faX;
+  public faComments = faComments;
+  public faRedo = faRedo;
+  public faPlayCircle = faPlayCircle;
+}
